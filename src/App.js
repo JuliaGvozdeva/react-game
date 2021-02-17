6357197c15879a5de4d0d55158b1e3ce84f5b163
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2021-02-17 23:07:54 +0400

    feat: create react-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Game from './components/Game/Game'
+import commonStyles from './common/styles/common'
+
+function App() {
+  return (
+    <div className={commonStyles.app}>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
