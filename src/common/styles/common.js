5c2b5ec8e520994a7c4d92cdd3cbfb4663ed00de
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2021-02-17 23:13:55 +0400

    feat: add main structure for project

---
+++ src/common/styles/common.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core';
+
+export const stylesCommonObj = {
+  body: {
+    backgroundImage: '../../../public/images/bg_tries.jpg'
+  },
+};
+
+const stylesCommon = makeStyles(stylesCommonObj);
+
+export default stylesCommon;
